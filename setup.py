bf78e73506f50c47b978c74e73b2acc8d34855ff
404 <141468084+qkc404@users.noreply.github.com>
2026-09-07 16:29:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="saeka",
-    version="1.8.0",
+    version="1.8.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={
