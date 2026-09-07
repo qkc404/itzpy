5bd401d04eca1ea76d303dec6ed5dda6f7669a16
404 <141468084+qkc404@users.noreply.github.com>
2026-09-07 15:52:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="saeka",
-    version="1.4.0",  # Bump version for the new release
+    version="1.5.0",  # Bump version for the new release
     description="Saeka GCP Cloud Run Deployer",
     author="Saeka Tojirp",
     packages=find_packages(),
